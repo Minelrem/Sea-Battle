a640609ed79973e54703e4536717b4f0195ce8b4
Minelrem <millenium.minelrem@gmail.com>
2017-10-17 17:50:51 +0300

    SeaBattle first commit

---
+++ SeaBattle.Data/Context/SeaBattleContext.cs
@@ -0,0 +1,21 @@
+﻿using SeaBattle.Data.Model;
+using System.Data.Entity;
+
+namespace SeaBattle.Data.Context
+{
+    public class SeaBattleContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserAccount> UserAccounts { get; set; }
+
+        public SeaBattleContext(): base("SeaBattleConnection")
+        {
+            
+        }
+
+        public SeaBattleContext(string connectionStringOrName): base(connectionStringOrName)
+        {
+            
+        }
+    }
+}
